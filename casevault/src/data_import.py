81be88cf86517940fd753728fdf1713d9b013a0b
Trent Swanson <trentmswanson@hotmail.com>
2017-02-06 11:58:27 -0500

    fix missing character bug

--- casevault/src/data_import.py
+++
@@ -1,9 +0,0 @@
-import adal
-
-context = adal.AuthenticationContext('https://login.microsoftonline.com/fs180.onmicrosoft.com')
-RESOURCE = 'f123a339-be25-420f-a843-ecad0938a050'
-token = context.acquire_token_with_username_password (
-    RESOURCE,
-    "test@fs180.onmicrosoft.com", 
-    "Green2015!",
-    "f123a339-be25-420f-a843-ecad0938a050")
